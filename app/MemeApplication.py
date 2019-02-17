3cd1780fa7b6d38b8cb0b05e5ce1abb6dc892400
Soochin Kang <elvin9792@gmail.com>
2019-02-16 17:46:00 -0800

    Reorganized as local app

---
+++ app/MemeApplication.py
@@ -0,0 +1,8 @@
+import tkinter
+
+class MemeApplication:
+    def __init__(self):
+        pass
+    
+    def run(self):
+        pass
---
+++ app/prawdata.py
@@ -0,0 +1,40 @@
+import praw
+
+from prawcore import NotFound
+
+MAX_POSTS = 25
+
+reddit = praw.Reddit(client_id = "BuHNPoZ-G1q9QQ",
+                     client_secret = "-Da1eZwtF5bizD6QivESFiqCa2w",
+                     user_agent = "Traditional_Bridge_")
+
+
+def subreddit_exists(subreddit):
+    exists = True
+    try:
+        reddit.subreddits.search_by_name(subreddit, exact=True)
+    except NotFound:
+        exists = False
+    return exists
+
+def get_subreddit(subreddit):
+    if not subreddit_exists(subreddit):
+        return {}
+
+    subreddit = reddit.subreddit(subreddit)
+
+    submissions = {"submissions":list()}
+
+    for submission in subreddit.hot(limit=MAX_POSTS):
+        submission_info = {"title": submission.title, "link": submission.shortlink, "id": submission.id}
+        submissions["submissions"].append(submission_info)
+
+    submissions
+
+def get_content(post_id):
+    post = reddit.submission(id=post_id)
+    contents = dict()
+    contents['title'] = post.title
+    contents['selftext'] = post.selftext
+
+    contents
