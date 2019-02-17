cfbf512568b63e739ab50dad4c02d4becd8e0921
Soochin Kang <elvin9792@gmail.com>
2019-02-17 00:17:33 -0800

    GUI implementing...

--- app/MemeApplication.py
+++ app/MemeApplication.py
@@ -1,12 +1,34 @@
 import tkinter
+import meme
+from PIL import Image, ImageTk
+
+DEFAULT_FONT = ('Helvetica', 30)
 
 class MemeApplication:
     def __init__(self):
         self._root_window = tkinter.Tk()
-        self._create_canvas()
+        self._root_window.title("Gimme a Dog!!!")
+        self._root_window.geometry("600x600")
 
-    def _create_canvas(self):
-        self._canvas = tkinter.Canvas(master = self._root_window, width=200, height=200)
+        self._button = tkinter.Button(master=self._root_window, text="Doggo Waiting V'ㅈ'V",
+                                      font=DEFAULT_FONT,
+                                      command=self._on_button_clicked, height=10)
+
+        # self._canvas = tkinter.Canvas(master = self._root_window, width=500, height=500)
+        # self._canvas.grid(row=0, column=0, padx=10, pady=10,
+        #                  sticky=tkinter.N + tkinter.S)
+        self._button.grid(row=1, column=0, columnspan=1, padx=10, pady=10, sticky=tkinter.N)
+
+
+    def _on_button_clicked(self) -> None:
+        print("Clicked")
+        img = meme.get_meme()
+        text = meme.pick_title()
+
+        tkimage = ImageTk.PhotoImage(img)
+        label = tkinter.Label(self._root_window, image=tkimage,
+                              text=text, compound=tkinter.CENTER)
+        label.grid(row=0, column=0, padx=10, pady=10)
     
     def run(self):
         self._root_window.mainloop()
--- app/meme.py
+++ app/meme.py
@@ -4,7 +4,6 @@ from PIL import ImageDraw
 from PIL import ImageFile
 ImageFile.LOAD_TRUNCTATED_IMAGES=True
 
-import os
 import requests
 from photourl import get_urls
 from prawdata import get_subreddit_titles
@@ -16,8 +15,7 @@ def pick_url():
 def pick_title():
     return random.choice(get_subreddit_titles("dogshowerthoughts"))
 
-
-def show_meme():
+def get_meme():
     with open('pic1.jpg', 'wb') as handle:
         response = requests.get(pick_url(), stream=True)
 
@@ -34,15 +32,13 @@ def show_meme():
 
         try:
             img = Image.open(image_name)
+            img.load()
         except IOError:
-            print("IOError")
             pass
 
-        draw = ImageDraw.Draw(img)
-        font = ImageFont.truetype("Roboto-Black.ttf", 100)
-        text = pick_title()
-        draw.text((0, 0),text,(255,255,255),font=font)
-        img.show()
-
-
-show_meme()
+        return img
+        # draw = ImageDraw.Draw(img)
+        # font = ImageFont.truetype("Roboto-Black.ttf", 120)
+        # text = pick_title()
+        # draw.text((0, 0),text,(255,255,255),font=font)
+        # img.show()
